46274092fd6f1ece8115c4a14b423395efa570b5
ytangamplus <y.tang.amplus@gmail.com>
2023-04-05 16:02:43 +0900

    プロジェクト ファイルを追加します。

---
+++ 4.4homework(while)/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace _4._4homework_while_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAlert = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(351, 461);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(140, 34);
+            this.btnPlus.TabIndex = 0;
+            this.btnPlus.Text = "Plus";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(351, 0);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(140, 440);
+            this.txtResult.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(562, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 75);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "「1+2+3+...」と足していき、\r\n合計が「500」超えたら、\r\n足さなくなるプログラム";
+            // 
+            // txtAlert
+            // 
+            this.txtAlert.Location = new System.Drawing.Point(12, 316);
+            this.txtAlert.Multiline = true;
+            this.txtAlert.Name = "txtAlert";
+            this.txtAlert.Size = new System.Drawing.Size(299, 90);
+            this.txtAlert.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 558);
+            this.Controls.Add(this.txtAlert);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnPlus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAlert;
+    }
+}
+
---
+++ 4.4homework(while)/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4._4homework_while_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            this.txtResult.Text = string.Empty;
+            int num = 1;
+            int sum = 1;
+            while (true)
+            {   
+                
+                this.txtResult.Text += (sum-num).ToString() + "+" + num.ToString()+ "=" + sum.ToString() + Environment.NewLine;
+                num++;
+                sum += num;
+                if (sum > 500)
+                {
+                    this.txtAlert.Text = "もう一回足すと500に超えてしまう!" + Environment.NewLine + "合計:" + (sum-num).ToString();
+                    break;
+                }
+
+
+
+            }
+
+        }
+    }
+}
